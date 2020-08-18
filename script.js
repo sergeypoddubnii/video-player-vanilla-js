5db8a3560a41af7371fd52518a16cba06ba194df
sergey <sergey_pidd@ukr.net>
2020-08-18 20:33:21 +0300

    add progress logic add timeupdate logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//refs
 const refs = {
   play: document.querySelector("#play"),
   stop: document.querySelector("#stop"),
@@ -6,13 +7,30 @@ const refs = {
   timestamp: document.querySelector("#timestamp"),
 };
 
+// helpers
+const getMinutes = (time) => {
+  let minutes = Math.floor(time / 60);
+  if (minutes < 10) {
+    return "0" + String(minutes);
+  }
+  return minutes;
+};
+
+const getSeconds = (time) => {
+  let seconds = Math.floor(time % 60);
+  if (seconds < 10) {
+    return "0" + String(seconds);
+  }
+  return seconds;
+};
+
+// callbacks
 const toggleVideoStatus = () => {
   if (refs.video.paused) {
     refs.video.play();
   } else {
     refs.video.pause();
   }
-  console.log("toggle");
 };
 
 const updatePlayIcon = () => {
@@ -23,18 +41,31 @@ const updatePlayIcon = () => {
   }
 };
 
+const setProgress = () => {
+  const minutes = getMinutes(refs.video.currentTime);
+  const seconds = getSeconds(refs.video.currentTime);
+
+  refs.progress.value = (refs.video.currentTime / refs.video.duration) * 100;
+  refs.timestamp.innerHTML = `${minutes}:${seconds}`;
+};
+
 const stopVideo = () => {
   refs.video.currentTime = 0;
   refs.video.pause();
 };
 
+const setVideoProgress = () => {
+  refs.video.currentTime =
+    (Number(refs.progress.value) * refs.video.duration) / 100;
+};
+
+//events
 refs.video.addEventListener("click", toggleVideoStatus);
 refs.video.addEventListener("play", updatePlayIcon);
 refs.video.addEventListener("pause", updatePlayIcon);
 refs.video.addEventListener("timeupdate", setProgress);
 
 refs.play.addEventListener("click", toggleVideoStatus);
-
 refs.stop.addEventListener("click", stopVideo);
 
 refs.progress.addEventListener("change", setVideoProgress);
